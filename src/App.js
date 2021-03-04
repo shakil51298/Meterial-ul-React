0de5622547da74a9366447a2fd693c3474002726
Md Khalid Hossain <zikaiwen02@gmail.com>
2021-03-03 23:34:27 -0800

    using axios

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import { Button } from '@material-ui/core';
 import News from './Components/News/News';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [article, setArticle] = useState([]);
 
-  useEffect(()=>{
-    const url = 'http://newsapi.org/v2/everything?q=tesla&from=2021-02-03&sortBy=publishedAt&apiKey=0ffd00b2d993414c85d69d8283c226e5'
-    fetch(url)
-    .then(res => res.json())
-    .then(data => setArticle(data.articles))
-  },[])
+  // useEffect(()=>{
+  //   const url = 'http://newsapi.org/v2/everything?q=tesla&from=2021-02-03&sortBy=publishedAt&apiKey=0ffd00b2d993414c85d69d8283c226e5'
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => setArticle(data.articles))
+  // },[])
+
+  useEffect(() => {
+    const url ="http://newsapi.org/v2/everything?q=tesla&from=2021-02-03&sortBy=publishedAt&apiKey=0ffd00b2d993414c85d69d8283c226e5"
+    axios(url)
+    .then(data => setArticle(data.data.articles))
+  }, [])
   return (
     <div className="App">
       <h3>Article = {article.length}</h3>
